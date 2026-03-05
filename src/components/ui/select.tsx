b3e85da156e847c51f4547b250976d386312fbef
Viransh Shah <shahv47@mcmaster.ca>
2026-03-04 22:09:53 -0500

    feat: add recurring unit type and validation for event edit

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -21,7 +21,7 @@ import { Text } from './text';
 
 const selectTv = tv({
   slots: {
-    container: 'mb-0',
+    container: 'mb-2',
     label: 'text-grey-100 mb-1 text-lg dark:text-neutral-100',
     input:
       'border-grey-50 mt-0 flex-row items-center justify-center rounded-xl border-[0.5px] px-3 py-2 dark:border-neutral-500 dark:bg-neutral-800',
