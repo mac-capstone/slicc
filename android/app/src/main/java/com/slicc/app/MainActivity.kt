c1830692bfdf1428309b0a6693affc851ba48709
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2026-03-14 20:14:20 -0400

    feat: setup google auth

--- android/app/src/main/java/com/slicc/app/MainActivity.kt
+++ android/app/src/main/java/com/slicc/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.development
+package com.slicc.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- src/lib/auth/utils.tsx
+++ src/lib/auth/utils.tsx
@@ -1,12 +1,20 @@
 import { getItem, removeItem, setItem } from '@/lib/storage';
 
 const TOKEN = 'token';
+const USER_ID = 'userId';
 
 export type TokenType = {
   access: string;
   refresh: string;
 };
 
-export const getToken = () => getItem<TokenType>(TOKEN);
-export const removeToken = () => removeItem(TOKEN);
-export const setToken = (value: TokenType) => setItem<TokenType>(TOKEN, value);
+export const getToken = (): TokenType | null => getItem<TokenType>(TOKEN);
+export const removeToken = (): void => removeItem(TOKEN);
+export const setToken = (value: TokenType): void =>
+  setItem<TokenType>(TOKEN, value);
+
+export const getUserIdFromStorage = (): string | null =>
+  getItem<string>(USER_ID);
+export const setUserIdInStorage = (value: string): void =>
+  setItem<string>(USER_ID, value);
+export const removeUserIdFromStorage = (): void => removeItem(USER_ID);
