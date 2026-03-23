934d4b79b94b7377dcfd70a44d4ab1a742139405
himikheal <himikheal@gmail.com>
2026-03-23 14:20:06 -0400

    fix: docID collision + backwards friend compatability
    also fix livesync in case signed out

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -138,40 +138,7 @@ export const parseReceiptInfo = (
 };
 
 export const encodeIdBase64Url = (value: string): string => {
-  const utf8FallbackEncode = (input: string): Uint8Array => {
-    const bytes: number[] = [];
-
-    for (const char of input) {
-      const codePoint = char.codePointAt(0);
-      if (codePoint === undefined) continue;
-
-      if (codePoint <= 0x7f) {
-        bytes.push(codePoint);
-      } else if (codePoint <= 0x7ff) {
-        bytes.push(0xc0 | (codePoint >> 6), 0x80 | (codePoint & 0x3f));
-      } else if (codePoint <= 0xffff) {
-        bytes.push(
-          0xe0 | (codePoint >> 12),
-          0x80 | ((codePoint >> 6) & 0x3f),
-          0x80 | (codePoint & 0x3f)
-        );
-      } else {
-        bytes.push(
-          0xf0 | (codePoint >> 18),
-          0x80 | ((codePoint >> 12) & 0x3f),
-          0x80 | ((codePoint >> 6) & 0x3f),
-          0x80 | (codePoint & 0x3f)
-        );
-      }
-    }
-
-    return Uint8Array.from(bytes);
-  };
-
-  const bytes =
-    typeof globalThis.TextEncoder === 'function'
-      ? new globalThis.TextEncoder().encode(value)
-      : utf8FallbackEncode(value);
+  const bytes = new TextEncoder().encode(value);
   const alphabet =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
   let output = '';
