746189f2cb6e599aa7cfa814ca78e5c8cecef78f
Asad <asadmhmhm@gmail.com>
2026-03-25 18:34:18 -0400

    fix: commited missed groups file

---
+++ src/app/(app)/groups.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { View } from '@/components/ui';
+
+export default function Groups() {
+  return (
+    <>
+      <View className="flex-1"></View>
+    </>
+  );
+}
